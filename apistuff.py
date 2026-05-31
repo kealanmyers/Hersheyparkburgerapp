d1dc9508de76d6cc411c09dcffb7cd2712aba843
Kealan Myers <kealanmyers@gmail.com>
2026-05-31 02:51:47 -0400

    lots of fixes, api implemented to main

--- apistuff.py
+++ apistuff.py
@@ -2,4 +2,14 @@ import requests as r
 url = "https://queue-times.com/parks/15/queue_times.json"
 response = r.get(url)
 data = response.json()
-print(data)
+
+ridelookup = {
+
+}
+
+for land in data["lands"]:
+    for ride in land["rides"]:
+        ridelookup[ride["name"].strip().lower()] = ride
+
+print(ridelookup["skyrush"])
+    
--- hershey.py
+++ hershey.py
@@ -1,7 +1,9 @@
 import math
+import requests as r
+from operator import itemgetter
 
-#create dicr of all hershey rides, noting ride type and grid cords
-#overlay a grid onto hershey park map to assign all rides an x and y value for cords
+#create dict of all hershey rides, noting grid cords
+#assign all rides an x and y value for cords
 rides = {
     "Candymonium": {"x": 4040, "y": 2210},
     "Carrousel": {"x": 3830, "y": 2390},
@@ -32,10 +34,12 @@ rides = {
 
 def main():
     #determine user's location
-    location = str(input("What ride are you at? ").strip())
+    location = str(input("What ride are you at? ").strip().lower)
     location = location.title()
     #determine the closest rides to user
-    getclose(location)
+    sixnames = getclose(location)
+    getwait(sixnames)
+    
 
 #sort obtained type of ride by distance, closest to farthest
 #print wait time along with name of ride in order of closest to furthest
@@ -50,44 +54,49 @@ def getclose(location):
     x, y = getcords(location)
     #creates a dict with the distance from current ride to every other ride
     ridedistance = {
-    "Candymonium": {getdistance(x, y, 4040, 2210)},
-    "Carrousel": {getdistance(x, y, 3830, 2390)},
+    "Candymonium": round(getdistance(x, y, 4040, 2210)),
+    "Carrousel": round(getdistance(x, y, 3830, 2390)),
 
-    "Skyrush": {getdistance(x, y, 3660, 1430)},
-    "Comet": {getdistance(x, y, 3380, 1570)},
-    "SooperDooperLooper": {getdistance(x, y, 3820, 1260)},
+    "Skyrush": round(getdistance(x, y, 3660, 1430)),
+    "Comet": round(getdistance(x, y, 3380, 1570)),
+    "SooperDooperLooper": round(getdistance(x, y, 3820, 1260)),
 
-    "Kissing Tower": {getdistance(x, y, 3190, 760)},
-    "Great Bear": {getdistance(x, y, 3310, 950)},
-    "Coal Cracker": {getdistance(x, y, 3460, 820)},
+    "Kissing Tower": round(getdistance(x, y, 3190, 760)),
+    "Great Bear": round(getdistance(x, y, 3310, 950)),
+    "Coal Cracker": round(getdistance(x, y, 3460, 820)),
 
-    "Trailblazer": {getdistance(x, y, 2780, 1480)},
-    "Storm Runner": {getdistance(x, y, 2420, 1490)},
-    "Jolly Rancher Remix": {getdistance(x, y, 2210, 1660)},
-    "Mix'd Flavored By Jolly Rancher": {getdistance(x, y, 2560, 1810)},
+    "Trailblazer": round(getdistance(x, y, 2780, 1480)),
+    "Storm Runner": round(getdistance(x, y, 2420, 1490)),
+    "Jolly Rancher Remix": round(getdistance(x, y, 2210, 1660)),
+    "Mix'd Flavored By Jolly Rancher": round(getdistance(x, y, 2560, 1810)),
 
-    "Wildcat's Revenge": {getdistance(x, y, 1900, 2490)},
-    "Wild Mouse": {getdistance(x, y, 1580, 2290)},
-    "Laff Trakk": {getdistance(x, y, 1370, 2630)},
-    "Lightning Racer": {getdistance(x, y, 1230, 2140)},
+    "Wildcat's Revenge": round(getdistance(x, y, 1900, 2490)),
+    "Wild Mouse": round(getdistance(x, y, 1580, 2290)),
+    "Laff Trakk": round(getdistance(x, y, 1370, 2630)),
+    "Lightning Racer": round(getdistance(x, y, 1230, 2140)),
 
-    "Ferris Wheel": {getdistance(x, y, 3070, 1560)},
-    "Reese's Cupfusion": {getdistance(x, y, 3260, 1810)},
-    "Cocoa Cruiser": {getdistance(x, y, 2940, 1730)},
-    "Twizzlers Twisted Gravity": {getdistance(x, y, 3190, 1340)},
+    "Ferris Wheel": round(getdistance(x, y, 3070, 1560)),
+    "Reese's Cupfusion": round(getdistance(x, y, 3260, 1810)),
+    "Cocoa Cruiser": round(getdistance(x, y, 2940, 1730)),
+    "Twizzlers Twisted Gravity": round(getdistance(x, y, 3190, 1340)),
 }
+    
     #sorts ridedistance from shortest to longerst distance
-    sorteddistance = dict(sorted(ridedistance.items(), key=lambda item: item[1]))
-
+    sorteddistance = dict(sorted(ridedistance.items(), key=itemgetter(1)))
+    
     #gathers the shortest 6 distances, excluding the current ride, returns them
-    closesix = [
-
+    closesixnames = [
+    # make sure this is empty when calling getclose()
     ]
     for i in sorteddistance:
-        if len(closesix) < 6:
+         if i == location:
+             pass
+         elif len(closesixnames) < 6:
+             closesixnames.append(i)
+
+    return(closesixnames)
 
     
-
 def square(n):
     return(n * n)
 
@@ -95,9 +104,27 @@ def getdistance(x1, y1, x2, y2):
     return(math.sqrt((square(x1 - x2)) + (square(y1 - y2))))
 
 
-def getwait():
-    #access online wait time information from hershey website for specific rides
-    pass
+def getwait(sixnames):
+    url = "https://queue-times.com/parks/15/queue_times.json"
+    response = r.get(url)
+    data = response.json()
+
+    ridelookup = {
+
+    }
+
+    for land in data["lands"]:
+        for ride in land["rides"]:
+            ridelookup[ride["name"].strip().lower()] = ride
+
+    for ridename in sixnames:
+        key = ridename.lower()
+        if ride['is_open']:
+            print(f"{ridename}: {ridelookup[key]['wait_time']} min wait")
+        else:
+            print(f"{ridename}: Ride is closed")
+        
 
 
-getclose("Candymonium")
+
+main()
